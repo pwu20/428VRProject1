5fa66163f2c7cdb3d59c68b7b6ad8a8b33b57f54
Peter Wu <59982014+pwu20@users.noreply.github.com>
2020-09-12 21:52:40 -0500

    ver alpha 0.2

---
+++ Assets/date1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class date1 : MonoBehaviour
+{
+    public GameObject dateTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    InvokeRepeating("UpdateDate", 0f, 10f);   
+    }
+
+    // Update is called once per frame
+    void UpdateDate()
+    {
+    dateTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("M/d/yy");
+       
+    }
+}
